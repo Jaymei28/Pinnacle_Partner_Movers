c8898ff60dafdbf6ea1dc30f6cd113db3c981430
Jaymei28 <jaymeirey.cortez@msunaawan.edu.ph>
2026-02-17 21:02:26 +0800

    Prioritize local data dump over CSV imports

--- backend/build.sh
+++ backend/build.sh
@@ -8,7 +8,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # One-time setup tasks
-python import_jobs_from_csv.py
-python import_carriers.py
+# python import_jobs_from_csv.py
+# python import_carriers.py
 python import_local_dump.py
 python create_admin.py
