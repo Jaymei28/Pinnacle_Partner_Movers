cfe1a938d8fb532a0ef10c02f247b7ae88d873f5
Jaymei28 <jaymeirey.cortez@msunaawan.edu.ph>
2026-02-17 20:33:15 +0800

    Automate job import and admin creation in build script

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# One-time setup tasks
+python import_jobs_from_csv.py
+python create_admin.py
