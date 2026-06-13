99b2887f299388646083b27857101127dcf3f837
Jaymei28 <jaymeirey.cortez@msunaawan.edu.ph>
2026-06-13 10:00:26 +0800

    fix: move db migrations to preDeployCommand

--- backend/build.sh
+++ backend/build.sh
@@ -5,10 +5,3 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
-
-# One-time setup tasks
-# python import_jobs_from_csv.py
-# python import_carriers.py
-python import_local_dump.py
-python create_admin.py
